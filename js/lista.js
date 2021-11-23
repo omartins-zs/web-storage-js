0d04fe295e09321c32e47e13b0c41ba884beacc9
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-23 09:17:30 -0300

    Adicionando metodo de Deletar e Executando

--- js/lista.js
+++ js/lista.js
@@ -4,6 +4,9 @@ let novoItem = document.querySelector('#novoItem');
 let btnNovoItem = document.querySelector('#btnNovoItem');
 let items = document.querySelector('#items');
 
+recuperarLista();
+
+// Podendo mudar o localStorage -> sessionStorage
 
 btnNovoItem.addEventListener('click', () => {
     inserirItem({ nome: novoItem.value, id: gerarId() });
@@ -30,4 +33,27 @@ function inserirItem(item, novoItem = true) {
     if (novoItem) {
         localStorage.setItem('listaDeCompras', JSON.stringify(lista));
     }
+}
+
+function criarItemLista(item) {
+    let li = document.createElement('li');
+    let btnHtml = '<button onClick="deletarItem(' + item.id + ')">Deletar</button>';
+    li.innerHTML = item.nome + '&nbsp;&nbsp;' + btnHtml;
+    li.style.marginBottom = '15px';
+    li.id = item.id;
+    return li;
+}
+
+function deletarItem(id) {
+
+    let indice = lista.findIndex(i => i.id == id);
+    if (indice < 0) {
+        alert('O ID não foi encontrado');
+        return;
+    }
+
+    lista.splice(indice, 1);
+    localStorage.setItem('listaDeCompras', JSON.stringify(lista));
+
+    document.getElementById('' + id + '').remove();
 }
